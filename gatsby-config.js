41f5839df8fe6b902f18630b65d20a288bbfb09b
YutoNakano <namawasabi1229@gmail.com>
2020-05-10 16:46:41 +0900

    faviconの調整

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
+        name: `shuffle-snowbarding`,
+        short_name: `shuffle`,
         start_url: `/`,
         background_color: fullConfig.theme.colors.white,
         theme_color: fullConfig.theme.colors.teal["400"],
         display: `minimal-ui`,
-        icon: `src/images/img/shufflenewlogo-white.png`,
+        icon: `src/images/icon.png`,
       },
     },
     {
