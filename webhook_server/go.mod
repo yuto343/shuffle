dd3250f36d1b85444a0bc791f6a3da6f47ffaf55
kotaroooo0 <k33asby@gmail.com>
2020-05-18 23:24:56 +0900

    Get microCMS webhook request

---
+++ webhook_server/go.mod
@@ -0,0 +1,3 @@
+module github.com/Yuto-Nakano/shuffle/webhook_server
+
+go 1.13
---
+++ webhook_server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type MicroCMSWebhookRequestBody struct {
+	Service string `json:"service"`
+	Api     string `json:"api"`
+	Id      string `json:"id"`
+	Type    string `json:"type"`
+}
+
+func webhookHandler(w http.ResponseWriter, req *http.Request) {
+	// リクエストをバリデートする
+	if req.Method != "POST" {
+		log.Fatal("error: invalid request method")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if req.Header.Get("Content-Type") != "application/json" {
+		log.Fatal("error: Invalid request content type")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer req.Body.Close()
+
+	microCMSWebhookRequestBody := MicroCMSWebhookRequestBody{}
+	if err := json.Unmarshal(body, &microCMSWebhookRequestBody); err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: MicroCMSのAPIを叩き最新記事情報を取得する
+	// TODO: 短時間に連投された記事を見分けられたりすると尚良い
+
+	// TODO: responseが5分返ってこないのはよくないので、リクエストが来たらジョブキューにいれるなどしてレスポンスを返す実装にするとよさそう
+	time.Sleep(5 * time.Minute) // 記事が実際にデプロイされるまで3分程度かかるためスリープする
+
+	// TODO: Twitter APIを叩き、ブログ記事についてのツイートする
+}
+
+func main() {
+	port, _ := strconv.Atoi(os.Args[1])
+	fmt.Printf("Starting server at Port %d", port)
+	http.HandleFunc("/microcms_webhook", webhookHandler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
