6e106b7412d1ec8f394456de7d20347a50cd930e
kotaroooo0 <k33asby@gmail.com>
2020-05-19 23:21:35 +0900

    Recive webhook and tweet blog notification

--- webhook_server/main.go
+++ webhook_server/main.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/joho/godotenv"
 )
 
 type MicroCMSWebhookRequestBody struct {
@@ -16,6 +19,14 @@ type MicroCMSWebhookRequestBody struct {
 	Api     string `json:"api"`
 	Id      string `json:"id"`
 	Type    string `json:"type"`
+}
+
+type MicroCMSBlogResponse struct {
+	Id        string    `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Title     string    `json:"title"`
+	Sentence  string    `json:"sentence"`
 }
 
 func webhookHandler(w http.ResponseWriter, req *http.Request) {
@@ -26,7 +37,7 @@ func webhookHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	if req.Header.Get("Content-Type") != "application/json" {
-		log.Println("error: Invalid request content type")
+		log.Println("error: invalid request content type")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -49,15 +60,22 @@ func webhookHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// TODO: MicroCMSのAPIを叩き最新記事情報を取得する
-	// TODO: 短時間に連投された記事を見分けられたりすると尚良い
-	req, _ := http.NewRequest("GET", "https://shuffle-snow.microcms.io/api/v1/blogs", nil)
-	req.Header.Set("X-API-KEY", os.Getenv("MICROCMS_API_KEY"))
+	if microCMSWebhookRequestBody.Type != "new" {
+		log.Println("error: invalid webhook type")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// MicroCMS側が最新のブログ記事を返すようになるまでしばらく時間がかかる
+	time.Sleep(5 * time.Second)
+
+	blogReq, _ := http.NewRequest("GET", "https://shuffle-snow.microcms.io/api/v1/blogs/"+microCMSWebhookRequestBody.Id, nil)
+	blogReq.Header.Set("X-API-KEY", os.Getenv("X_API_KEY"))
 
 	client := new(http.Client)
-	resp, err := client.Do(req)
+	resp, err := client.Do(blogReq)
 
-	body, err := ioutil.ReadAll(resp.Body)
+	blogBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -65,10 +83,46 @@ func webhookHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	defer resp.Body.Close()
 
+	microCMSBlogResponse := MicroCMSBlogResponse{}
+	if err := json.Unmarshal(blogBody, &microCMSBlogResponse); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	tweetContent := "新しい記事が投稿されました #shuffle_snowboarding\n"
+	tweetContent += microCMSBlogResponse.Title + " - @shuffle_DU\n"
+	tweetContent += "https://www.shuffle-snowboarding.style/blogs/" + microCMSBlogResponse.Id
+
 	// TODO: Twitter APIを叩き、ブログ記事についてのツイートする
+	api := getTwitterApi()
+	tweet, err := api.PostTweet(tweetContent, nil)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(tweet.Text)
+
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+func getTwitterApi() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("CONSUMER_SECRET"))
+	return anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN_KEY"), os.Getenv("ACCESS_TOKEN_SECRET"))
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
 
 func main() {
+	loadEnv()
 	port, _ := strconv.Atoi(os.Args[1])
 	fmt.Printf("Starting server at Port %d", port)
 	http.HandleFunc("/microcms_webhook", webhookHandler)
